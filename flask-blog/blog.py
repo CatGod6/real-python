4b8fa796b2fe9c0b2963575000528a5f0b5d5081
Catrell Washington <catrellio12@gmail.com>
2016-06-29 15:24:55 -0500

    Made some AWESOME Changes bruH

--- flask-blog/blog.py
+++ flask-blog/blog.py
@@ -2,7 +2,7 @@
 
 from flask import Flask, render_template, request, session, flash, redirect, url_for, g
 import sqlite3
-
+from functools import wraps
 #Let's config the database.
 
 DATABASE = 'blog.db'
@@ -18,30 +18,46 @@ app.config["DEBUG"] = True
 
 app.config.from_object(__name__)
 
+#Connects to database
 def connect_db():
 	return sqlite3.connect(app.config['DATABASE'])
 
+#Makes the user aware that you HAVE to login
+def login_required(test):
+	@wraps(test)
+	def wrap(*args, **kwargs):
+		if 'logged_in' in session:
+			return test(*args, **kwargs)
+		else: 
+			flash("You need to login first!")
+			return redirect(url_for('login'))
+	return wrap
+
+#routes to the login page and checks if credentials are right
 @app.route('/', methods=['GET', 'POST'])
 def login():
 	error = None
 	if request.method == 'POST':
-		if request.form['username'] != app.config['USERNAME']or \
+		if request.form['username'] != app.config['USERNAME'] or \
 		       request.form['password'] != app.config['PASSWORD']:
 		       error = 'Invalid Credentials. Please try again'
 		else:
-		   	session['logged in'] = True 
+		   	session['logged_in'] = True 
 		   	return redirect(url_for('main'))
 	return render_template('login.html', error=error)
 
-@app.route('/logout')
-def logout():
-	seesion.pop('logged_in',None)
-	flash('You were logged out!')
-	return redirect(url_for('login'))
-
 @app.route('/main')
+@login_required
 def main():
 	return render_template('main.html')
 
+#routes to the logout page and gives the user a message
+@app.route('/logout')
+def logout():
+	session.pop('logged_in', None)
+	flash('You were logged out!')
+	return redirect(url_for('login'))
+
+#Initiates the website in DEBUGGER mode
 if __name__ == "__main__" :
 	app.run()
